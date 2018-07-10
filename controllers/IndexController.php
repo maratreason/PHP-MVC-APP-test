59ae6450880221b8f8139f151eb63d19325b2c43
maratreason <maratreason@mail.ru>
2018-07-10 14:45:30 +0300

    10.07.2018

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -11,7 +11,21 @@ class IndexController extends Controller {
 
 	public function index() {
 		$this->pageData['title'] = "Вход в личный кабинет";
+
+		if (!empty($_POST)) {
+			if (!$this->login()) {
+				$this->pageData['error'] = "Неправильный логин или пароль";
+			}
+		}
+
 		$this->view->render($this->pageTpl, $this->pageData);
+
+	}
+
+	public function login() {
+		if (!$this->model->checkUser()) {
+			return false;
+		}
 	}
 
 }
--- views/main.tpl.php
+++ views/main.tpl.php
@@ -13,26 +13,24 @@
 	<header></header>
 
 	<div id="content">
-		<div class="container">
-			<div class="row">
+		<div class="container table-block">
+			<div class="row table-cell-block">
 				<div class="col-sm-6 col-md-4 col-md-offset-4">
-					<h1 class="text-center login-title">Sign in to continue to Bootsnipp</h1>
+					<h1 class="text-center login-title">Вход в кабинет</h1>
 					<div class="account-wall">
 						<img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
 						alt="">
-						<form class="form-signin">
-							<input type="text" class="form-control" placeholder="Email" required autofocus>
-							<input type="password" class="form-control" placeholder="Password" required>
+						<form class="form-signin" id="form-signin" method="post">
+						<?php if (!empty($pageData['error'])) : ?>
+							<p><?php echo $pageData['error']; ?></p>
+						<?php endif; ?>
+							<input type="text" class="form-control" name="login" id="login" placeholder="Логин" required>
+							<input type="password" class="form-control" name="password" id="password" placeholder="Пароль" required>
 							<button class="btn btn-lg btn-primary btn-block" type="submit">
-								Sign in</button>
-								<label class="checkbox pull-left">
-									<input type="checkbox" value="remember-me">
-									Remember me
-								</label>
+								Войти</button>
 								<a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
 							</form>
 						</div>
-						<a href="#" class="text-center new-account">Create an account </a>
 					</div>
 				</div>
 			</div>
@@ -43,7 +41,7 @@
 
 	<script src="/js/jquery.min.js"></script>
 	<script src="/js/bootstrap.min.js"></script>
-	<script src="/js/angular.min.js"></script>
+	<!-- <script src="/js/angular.min.js"></script> -->
 	<script src="/js/script.js"></script>
 </body>
 </html>
